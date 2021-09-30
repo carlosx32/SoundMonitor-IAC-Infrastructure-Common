381744191a5330252fa437b68345a72b38d5df28
Carlos David Camacho <carlos0camacho@gmail.com>
2021-09-29 21:48:21 -0500

    Update data.tf

--- use-cases/lambda_s3_kafka/data.tf
+++ use-cases/lambda_s3_kafka/data.tf
@@ -1,8 +1,8 @@
 data "aws_s3_bucket" "s-1" {
-  bucket = "soundmonitor-audiodata"
+  bucket = "soundmonitor-audiodata-663588064491"
 }
 
 data "aws_s3_bucket" "s-2" {
-  bucket = "soundmonitor-visualdata"
+  bucket = "soundmonitor-visualdata-663588064491"
 }
 
